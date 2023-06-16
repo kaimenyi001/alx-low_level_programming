bd2778a06a578b21af921e5d2d44fac53d7b15cb
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-16 14:44:57 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+		putchar(l);
+	putchar('\n');
+return (0);
+}
