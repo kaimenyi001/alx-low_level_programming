f449f3f31770ea2d2970b7fab1141df4d9406d4c
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-20 12:49:32 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ *
+ * @int: inputs character
+ *
+ * Return: 0 Success
+ */
+
+int _abs(int)
+{
+	int k;
+
+	if (k >= 0)
+	{
+	return (k);
+	}
+	else
+	{
+	return (k * -1);
+	}
+}
