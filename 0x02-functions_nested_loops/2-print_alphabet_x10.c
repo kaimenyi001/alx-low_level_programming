6299b1645520f98c2ad7410e43c77025c358dbfa
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-20 11:37:32 -0700

    times ten

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times  the alphabet
+ *
+ * Return: 0 Success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (i = 97; i < 123; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-putchar.c - a program that prints _putchar
 1-alphabet.c
+2-print_alphabet_x10.c - prints 10 times the alphabet
