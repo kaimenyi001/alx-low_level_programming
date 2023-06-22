96a9bf8af092ad67411c8c91fc23a3936bac7448
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-22 11:57:10 -0700

    numbers times ten

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10 times 0 to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int n, m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (c > 9)
+			_putchar((m / 10) + 0);
+			_putchar((m % 10) + 0);
+		}
+		_putchar('\n');
+	}
+}
+
