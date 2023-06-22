1a3f61bb6213752a77e0deab2f518c5157bcd070
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-22 12:27:04 -0700

    tired

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,15 @@
 
 void more_numbers(void)
 {
-	int n, m;
+	int n;
+	char m;
 
 	for (n = 0; n < 10; n++)
 	{
 		for (m = 0; m <= 14; m++)
 		{
 			if (c >= 10)
-			{
 			_putchar((m / 10) + '0');
-			}
 			_putchar((m % 10) + '0');
 		}
 		_putchar('\n');
