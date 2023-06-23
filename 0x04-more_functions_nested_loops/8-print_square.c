6f8488856364b07b2578f36d11f74dcf36cb8b6d
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-23 01:22:22 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,10 +13,8 @@ void print_square(int size)
 		_putchar('\n');
 	for (i = 0; i < size; i++)
 	{
-	for (j = 0; j < size; j++)
-	{
-		_putchar('#');
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
 	}
-	}
-	_putchar('\n');
 }
