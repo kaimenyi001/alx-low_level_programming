0338bd49eb81dce769b675bd651f8cfb11522372
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-26 09:21:43 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: inputs string
+ */
+
+void print_rev(char *s)
+{
+	int k;
+
+	for (k = 0; s[k] != '\0')
+	{
+	}
+	for (k = k - 1; k >= 0; k--)
+	{
+		_putchar(s[k]);
+	}
+	_putchar('\n');
+}
