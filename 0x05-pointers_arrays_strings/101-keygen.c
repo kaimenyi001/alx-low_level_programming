976c2e994e4527e915ad6cf5463666733c0c1ec2
kaimenyi001 <kaimenyi1@gmail.com>
2023-06-26 12:12:42 -0700

    random passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0, y = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (y < 2772)
+	{
+		x = rand() % 128;
+		if ((y + x) > 2772)
+			break;
+		y = y + x;
+		printf("%c", x);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
