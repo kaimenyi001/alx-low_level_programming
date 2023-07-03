8a87bc90f15d900d2678ec69e302763043428ab5
kaimenyi001 <kaimenyi1@gmail.com>
2023-07-03 12:27:08 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int _strspn - gets the length of a prefix substring
+ * @s: pointer
+ * @accept: array
+ * Return: void
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
