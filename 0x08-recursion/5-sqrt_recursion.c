afc389ac9ac7104e9fdefb65b01d06b052704b8a
kaimenyi001 <kaimenyi1@gmail.com>
2023-07-05 12:47:57 -0700

    sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * pwr_op - returns natural sqrt of a number
+ * @m: integer
+ * @n: integer
+ * Return: void
+ */
+
+int pwr_op(int m, int n)
+{
+	if (n % (m / n) == 0)
+	{
+		if (n * (m / n) == m)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + pwr_op(m, n + 1));
+}
+/**
+ * _sqrt_recursion -  returns the natural square root of a number
+ * @n: integer value
+ * Return: void
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (power_operation(n, 2));
+}
