8aa82ef70d7df4ba2391708aa04ff55a56f7e4f4
kaimenyi001 <kaimenyi1@gmail.com>
2023-07-07 13:41:53 -0700

    args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", *argc - 1);
+	return (0);
+}
