9f31db60a0835067ea3e9bd1fe2c6b3a91fcf723
kaimenyi001 <kaimenyi1@gmail.com>
2023-07-11 19:41:29 -0700

    strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include < stdlib.h>
+
+/**
+ * free_grid - frees array
+ * @grid: array of char
+ * @height: height of array
+ * Return: void
+ */
+
+void free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: array
+ * Return: void
+ */
+
+char **strtow(char *str)
+{
+	char **out;
+	unsigned int i, height, x, y, z;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (i = height = 0; str[i] != '\0'; i++)
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			height++;
+	out = malloc((height + 1) * sizeof(char *));
+	if (out == NULL || height == 0)
+	{
+		free(out);
+		return (NULL);
+	}
+	for (x = z = 0; x < height; x++)
+	{
+		for (i = z; str[i] != '\0'; i++)
+		{
+			if (str[i] == ' ')
+				z++;
+			if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			{
+				out[x] = malloc((i - z + 2) * sizeof(char));
+				if (out[x] == NULL)
+				{
+					free_grid(out, x);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		for (y = 0; z <= i; z++, y++)
+			out[x][y] = str[z];
+		out[x][y] = '\0';
+	}
+	out[x] = NULL;
+	return (out);
+}
