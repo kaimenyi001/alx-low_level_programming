c994812754a25e1efee092bca357f49bffd8800c
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-07-27 14:51:49 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t i;
 
-	nelem = 0;
+	i = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		i++;
 	}
-	return (nelem);
+	return (i);
 }
