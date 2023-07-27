5c9c1d1c5033c9d61f9f9b8f2c47805d6cc1dafc
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-07-27 06:46:47 -0700

    print list

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+Singly linked lists - When and why using linked lists vs arrays
+How to build and use linked lists
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,10 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+size_t print_list(const list_t *h);
+
+#endif
