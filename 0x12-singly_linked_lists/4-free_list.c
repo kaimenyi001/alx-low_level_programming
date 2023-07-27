196158ed95271c6aa943c9e1077a82548210a807
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-07-27 13:21:35 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * free_list -  frees a list_t list
+ * @head: head of the linked list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t cur;
+
+	while ((cur = head) != NULL)
+	{
+		head = head->next;
+		free(cur->str);
+		free(cur);
+	}
+}
