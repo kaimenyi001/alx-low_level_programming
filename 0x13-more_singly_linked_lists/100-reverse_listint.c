414cb2638d2853c956265e61c45ef53317c5cee6
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-08-01 12:15:50 -0700

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: head of list
+ * Return: void
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
