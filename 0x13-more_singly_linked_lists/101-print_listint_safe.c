0c96c744cf92ec8f40f7c049af61b6b7fd99266e
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-08-01 19:02:30 -0700

    safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,9 +1,68 @@
 #include "lists.h"
 
 /**
+ * free_lists - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_lists(lists_t **head)
+{
+	lists_t *temp, *c;
+
+	if (head == NULL)
+		return;
+	c = *head;
+	while (c != NULL)
+	{
+		temp = c;
+		c = c->next;
+		free(temp);
+	}
+	*head = NULL;
+}
+
+/**
  * print_listint_safe - prints a listint_t linked list.
  * @head: head of list
  * Return: void
  */
 
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+	lists_t *new, *p, *add;
 
+	p = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(lists_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->s = (void *)head;
+		new->next = p;
+		p = new;
+
+		add = p;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->s)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_lists(p);
+				return (i);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		i++;
+	}
+
+	free_lists(p);
+	return (i);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct lists_s - singly linked list
+ * @s: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct lists_s
+{
+	void *s;
+	struct lists_s *next;
+} lists_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
